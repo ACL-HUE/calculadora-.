ec9fff7404b0fa01bbc81a3d75a17eefc7454394
unknown <stgargar@ithwh.edu.gt>
2025-08-13 20:25:36 -0600

    calculadora

---
+++ Calculadora.py
@@ -0,0 +1,23 @@
+"""Esta es una calculadora funcional que permite realizar sumas, restas y multiplicar, este codigo esta elaborado por su servidor 
+Allan García el dia 13 de Agosto del 2025, se espera que sea de su total agrado feliz día"""
+def calculadora():#inicio 
+    print("elegir la operacion hacerse:")#elegir Operacion deseada 
+    print("1. Sumar")#eligio suma
+    print("2. Restar")#eligio resta 
+    print("3. Multiplicar")#eligio miltiplicacion
+    
+    operacion = input("Ingrese el número de la operación que desea (1,2,3): ")#ingresar el numero de operacion deseada 
+
+    num1 = float(input("Ingrese el primer número: "))#ingrese primer numero 
+    num2 = float(input("Ingrese el segundo número: "))#ingrese segundo numero 
+    if operacion == "1":#operacion suma
+        resultado = num1 + num2#numeros ingresados
+        print(f"El resultado de la suma es: {resultado}")#mostrando el resultado 
+    elif operacion == "2":#operacion resta
+        resultado = num1 - num2#numeros ingresados
+        print(f"El resultado de la resta es: {resultado}")#mostrando el resultado 
+    elif operacion == "3":#operacion multiplicacion 
+        resultado = num1 * num2#numeros ingresados 
+        print(f"El resultado de la multiplicación es: {resultado}")#mostrando el resultado
+
+calculadora()#fin de calculadora 
